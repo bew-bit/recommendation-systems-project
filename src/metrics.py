f1ddce5d4a0fdfb7ab4548d9d02b4a2b19649119
Svetchen <svetlana.reimer.job@gmail.ru>
2021-07-21 21:25:41 +0300

    project

---
+++ src/metrics.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def hit_rate(recommended_list, bought_list):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    flags = np.isin(bought_list, recommended_list)
+    return (flags.sum() > 0) * 1
+
+def hit_rate_at_k(recommended_list, bought_list, k=5):
+    return hit_rate(recommended_list[:k], bought_list)
+
+def precision(recommended_list, bought_list):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    flags = np.isin(bought_list, recommended_list)
+    return flags.sum() / len(recommended_list)
+
+def precision_at_k(recommended_list, bought_list, k=5):
+    return precision(recommended_list[:k], bought_list)
